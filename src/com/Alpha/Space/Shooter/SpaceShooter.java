f32d58daccc17252c3c48195e07370762f949108
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-03-20 12:51:18 -0600

    Added a fx class

---
+++ src/com/Alpha/Space/Shooter/SpaceShooter.java
@@ -0,0 +1,16 @@
+package com.Alpha.Space.Shooter;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class SpaceShooter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
