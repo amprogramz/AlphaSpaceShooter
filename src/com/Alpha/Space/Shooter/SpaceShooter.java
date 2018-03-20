4d49c7c33390458c4d81c15f4854f014918db1d6
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-03-20 15:13:54 -0600

    Here are some cool space backgrounds.

--- src/com/Alpha/Space/Shooter/SpaceShooter.java
+++ src/com/Alpha/Space/Shooter/SpaceShooter.java
@@ -3,7 +3,8 @@ package com.Alpha.Space.Shooter;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
-public class SpaceShooter extends Application {
+public class SpaceShooter extends Application
+{
 
     public static void main(String[] args) {
         launch(args);
