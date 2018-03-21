d752dc38495353e2599738de406daf168ed87f6c
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-03-20 22:29:42 -0600

    I added an Enemy() as a super class for enemies. Target is a child class of enemy.

---
+++ src/com/Alpha/Space/Shooter/Enemy.java
@@ -0,0 +1,44 @@
+package com.Alpha.Space.Shooter;
+
+import javafx.scene.shape.Rectangle;
+
+public class Enemy
+{
+    private Rectangle enemy = new Rectangle();
+    private int targetIndex;
+    private int hitPoints;
+
+    public Rectangle getEnemy()
+    {
+        return enemy;
+    }
+    public void setEnemy(Rectangle enemy)
+    {
+        this.enemy = enemy;
+    }
+    public int getIndex()
+    {
+        return targetIndex;
+    }
+    public void setTargetIndex(int index)
+    {
+        targetIndex = index;
+    }
+    public void setHitPoints(int hitPoints)
+    {
+        this.hitPoints = hitPoints;
+    }
+
+
+    public void destruct(int damage)
+    {
+        if(this.hitPoints > damage)
+        {
+            hitPoints = hitPoints - damage;
+        }else
+        {
+            //this line needs to be fixes to remove the enemy
+            enemy.setX(-200);
+        }
+    }
+}
--- src/com/Alpha/Space/Shooter/Target.java
+++ src/com/Alpha/Space/Shooter/Target.java
@@ -2,40 +2,22 @@ package com.Alpha.Space.Shooter;
 
 import javafx.scene.shape.Rectangle;
 
-public class Target
+public class Target extends Enemy
 {
-    private Rectangle target = new Rectangle();
-    private int targetIndex;
-    private int hitPoints = 30;
+
 
     public Target(double x, double y, int index)
     {
+        Rectangle target = new Rectangle();
         target.setX(x);
         target.setY(y);
         target.setWidth(100);
         target.setHeight(30);
-        targetIndex = index;
-    }
-    public Rectangle getBlock()
-    {
-        return target;
-    }
-    public int getIndex()
-    {
-        return targetIndex;
+        super.setEnemy(target);
+        super.setTargetIndex(index);
+        super.setHitPoints(30);
     }
 
-
-    public void destruct(int damage)
-    {
-        if(this.hitPoints > damage)
-        {
-            hitPoints = hitPoints - damage;
-        }else
-        {
-            target.setX(-200);
-        }
-    }
 
 
 }
