cb83e53f094b54c20802d344588389f2b12f114c
Alec McDaugale <amcdaugale@student.cccs.edu>
2018-04-10 12:03:57 -0600

    Added some methods with no definitions to Background to represent the idea we discussed in class.

--- src/com/Alpha/Space/Shooter/Background.java
+++ src/com/Alpha/Space/Shooter/Background.java
@@ -1,15 +1,40 @@
 package com.Alpha.Space.Shooter;
 
-
+//This is just the basic idea we discussed. we could also add a canvas for particle effects.
+import javafx.scene.canvas.Canvas;
 import javafx.scene.image.ImageView;
 
 public class Background
 {
     ImageView background;
+    Canvas moveableBackground = new Canvas();
     String filePath = "sprite/Gods-and-Idols-2012-04-11-21-40-17-86.jpg";
 
     public Background()
     {
         background = SpriteTool.getImage(filePath, 0, 0);
     }
+    public Background(String filePath)
+    {
+        background = SpriteTool.getImage(filePath, 0, 0);
+
+    }
+
+    public Canvas getBackground()
+    {
+        return moveableBackground;
+    }
+    public void setZoom()
+    {
+
+    }
+//    this is where we would call the animation, like timeline which calls move forward
+    public void setInMotion()
+    {
+
+    }
+    public void moveForward()
+    {
+    }
+
 }
