3829c8ac85b575e5d79a9961c5794a1eba2a7d6e
Quin Capener <quincapener@gmail.com>
2018-04-22 16:15:00 -0600

    Phaser, Moveable and sounds

---
+++ src/com/Alpha/Space/Shooter/Moveable.java
@@ -0,0 +1,6 @@
+package com.Alpha.Space.Shooter;
+
+public interface Moveable {
+
+	public void move(int movement);
+}
---
+++ src/com/Alpha/Space/Shooter/Phaser.java
@@ -0,0 +1,35 @@
+package com.Alpha.Space.Shooter;
+
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+
+public class Phaser extends Ammo{
+	
+	private String shotSound = "Sounds/Sound Effects/Laser Shot.wav";
+	
+	public Phaser() {
+		Rectangle ammo = new Rectangle();
+		ammo.setWidth(5);
+		ammo.setHeight(40);
+		ammo.setFill(Color.CORNFLOWERBLUE);
+		super.setRound(ammo);
+		super.setMovement(18.8);
+		super.setDamage(20);
+		super.setShotSound(SoundTool.getAudioClip(shotSound));
+	}
+
+	@Override
+	public void shipShoot(EnemyArray enemy, Score score) {
+        getRound().setLayoutY(getRound().getLayoutY() - getMovement());
+        enemyColishionCheck(enemy, score);
+		
+	}
+
+	@Override
+	public void enemyShoot(Ship ship, Score score) {
+		getRound().setLayoutY(getRound().getLayoutY() + getMovement());
+        shipColishionCheck(ship, score);
+		
+	}
+
+}
